28a27dc3a16ce6e4573f15c59203de7dc0f61548
yangchuantongStarry <18513181702@163.com>
2022-10-20 17:53:59 +0800

    Update S3 download url

--- uptick_7000-1/node.sh
+++ uptick_7000-1/node.sh
@@ -2,7 +2,7 @@
 
 #download package
 cd $HOME
-wget https://download.uptick.network/download/uptick/testnet/release/v0.2.3/v0.2.3.tar.gz
+wget http://download.uptick.network/download/uptick/testnet/release/v0.2.3/v0.2.3.tar.gz
 tar -zxvf v0.2.3.tar.gz
 chmod +x uptick-v0.2.3/linux/uptickd
 
@@ -21,7 +21,7 @@ uptickd init testnet --chain-id=uptick_7000-1
 #download data file
 cd  $HOME/.uptickd
 rm -rf data
-wget https://download.uptick.network/download/uptick/testnet/node/data/data.tar.gz
+wget http://download.uptick.network/download/uptick/testnet/node/data/data.tar.gz
 tar -zxvf data.tar.gz
 #update Configuration
 curl -o $HOME/.uptickd/config/config.toml https://raw.githubusercontent.com/UptickNetwork/uptick-testnet/main/uptick_7000-1/config.toml
